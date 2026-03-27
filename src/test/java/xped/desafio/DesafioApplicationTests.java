5da8a8f20fd8014c9aac2ad37b7c9e2899fc2a6f
JoãoMizael <joaomizael3712@gmail.com>
2026-03-27 09:47:32 -0300

    Primeiro Commit | h2

---
+++ src/test/java/xped/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package xped.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
