99c36f8761d44d76ed58d7117ebe8afba3768943
JoãoMizael <joaomizael3712@gmail.com>
2026-03-27 10:32:32 -0300

    CRUD cliente

---
+++ src/main/java/xped/desafio/Controller/ClienteController.java
@@ -0,0 +1,50 @@
+package xped.desafio.Controller;
+
+import xped.desafio.model.Cliente;
+import xped.desafio.service.ClienteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return clienteService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/nome/{nome}")
+    public List<Cliente> buscarPorNome(@PathVariable String nome) {
+        return clienteService.buscarPorNome(nome);
+    }
+
+    @GetMapping("/contar")
+    public long contarClientes() {
+        return clienteService.contarClientes();
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return clienteService.salvar(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+    }
+}
